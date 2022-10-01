b3ff91f3ef6f2012a4efea56c14c16f347d2f5e4
bolinx2003 <3154756364@qq.com>
2022-10-01 22:59:01 +0800

    提交代码

---
+++ 2022/test_10_1/test_10_1/test.c
@@ -0,0 +1,101 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//enum Sex
+//{
+//	MALE,
+//	FEMALE,
+//	SECRET
+//};
+//
+//enum Color
+//{
+//	RED,
+//	GREEN,
+//	BLUE
+//};
+//
+//int main()
+//{
+//	// 枚举常量
+//	enum Sex s = MALE;
+//	printf("%d %d %d", RED, GREEN, BLUE);
+//
+//	return 0;
+//}
+
+//#define M 100
+//
+//int main()
+//{
+//	// #define定义的标识符常量
+//	int a = M;
+//	int arr[M] = { 0 };
+//	printf("%d\n", a);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	// 字面常量
+//	//20;
+//	//3.14;
+//	//'a'; // 字符常量
+//	//"abcdef"; // 字符串常量
+//
+//	//int a = 10;
+//	//char ch = 'w';
+//
+//	// const修饰的常变量
+//	//const int num = 10;
+//	//printf("%d\n", num);
+//	//num = 20; // err
+//	//printf("%d\n", num);
+//	//const int n = 10;
+//	//int arr[n] = { 0 }; // err
+//
+//	return 0;
+//}
+
+#include <string.h>
+
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a', 'b', 'c', '\0' };
+//
+//	//printf("%s\n", arr1);
+//	//printf("%s\n", arr2);
+//
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//
+//	return 0;
+//}
+
+// 转义字符
+//int main()
+//{
+//	printf("abcdef\n");
+//	printf("abc\ndef\n");
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	//printf("(Are you ok\?\?)");
+//	//printf("%c", '\'');
+//	//printf("%s", "abcdef");
+//	//printf("%s", "a");
+//	//printf("%s", "\"");
+//	//printf("C:\\test\\test.c");
+//	//printf("\a");
+//	//printf("abc\tdef");
+//	//printf("%c", '\130');
+//	//printf("%c", '\x31');
+//
+//	return 0;
+//}
