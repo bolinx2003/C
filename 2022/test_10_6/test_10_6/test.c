e9df91718473a8faa8475ea0498b05cb0c1c64af
bolinx2003 <3154756364@qq.com>
2022-10-06 15:59:53 +0800

    提交代码

--- 2022/test_10_6/test_10_6/test.c
+++ 2022/test_10_6/test_10_6/test.c
@@ -155,3 +155,95 @@
 //	return 0;
 //}
 
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int m = 0;
+//	scanf("%d %d", &a, &b);
+//
+//	for (m = a; m % b != 0; m += a)
+//	{
+//		;
+//	}
+//
+//	printf("%d\n", m);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int count = 0;
+//	int y = 0;
+//	for (y = 1000; y <= 2000; y++)
+//	{
+//		if (y % 4 == 0)
+//		{
+//			if (y % 100 != 0)
+//			{
+//				++count;
+//				printf("%d ", y);
+//			}
+//		}
+//		if (y % 400 == 0)
+//		{
+//			++count;
+//			printf("%d ", y);
+//		}
+//	}
+//
+//	printf("\ncount = %d\n", count);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int count = 0;
+//	int y = 0;
+//	for (y = 1000; y <= 2000; y++)
+//	{
+//		if (((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0))
+//		{
+//			++count;
+//			printf("%d ", y);
+//		}
+//	}
+//
+//	printf("\ncount = %d\n", count);
+//
+//	return 0;
+//}
+
+#include <math.h>
+
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 101; i < 200; i += 2)
+//	{
+//		int flag = 1; // 假设i是素数
+//		int j = 0;
+//		for (j = 2; j <= sqrt(i); j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				// i不是素数
+//				flag = 0;
+//				break;
+//			}
+//		}
+//		if (1 == flag)
+//		{
+//			// i是素数
+//			printf("%d ", i);
+//			++count;
+//		}
+//	}
+//
+//	printf("\ncount = %d\n", count);
+//
+//	return 0;
+//}
