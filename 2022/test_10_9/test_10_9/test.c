eb1845081c573cfe3d9e1af272353ab60107df91
bolinx2003 <3154756364@qq.com>
2022-10-09 11:48:04 +0800

    提交代码

---
+++ 2022/test_10_9/test_10_9/test.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//#include "add.h"
+// µ¼Èë¾²Ì¬¿â
+//#pragma comment(lib, "add.lib")
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//
+//	int sum = Add(a, b);
+//
+//	printf("sum = %d\n", sum);
+//
+//	return 0;
+//}
+
+//void test(int* pa)
+//{
+//	(*pa)++;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	printf("a = %d\n", a);
+//
+//	test(&a);
+//	printf("a = %d\n", a);
+//
+//	test(&a);
+//	printf("a = %d\n", a);
+//
+//	test(&a);
+//	printf("a = %d\n", a);
+//
+//	return 0;
+//}
