21f0895cc770bb2f62f5f18eadb7a95b2e047caf
bolinx2003 <3154756364@qq.com>
2022-10-11 21:53:23 +0800

    提交代码

---
+++ 2022/test_10_11/test_10_11/test.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+// 大写转小写
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		if (ch >= 'A' && ch <= 'Z')
+//		{
+//			ch += 32;
+//		}
+//		printf("%c\n", ch);
+//		getchar(); // 处理\n
+//	}
+//
+//	return 0;
+//}
+
+// 使用函数打印乘法口诀表
+//void print_table(int n)
+//{
+//	int i = 1;
+//	for (; i <= n; ++i)
+//	{
+//		int j = 1;
+//		for (; j <= i; ++j)
+//		{
+//			printf("%d×%d=%d ", i, j, i * j);
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	print_table(n);
+//
+//	return 0;
+//}
+
