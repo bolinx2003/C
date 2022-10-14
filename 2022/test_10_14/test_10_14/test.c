51436ab99ef5feba0502d959da24671bf1e3856e
bolinx2003 <3154756364@qq.com>
2022-10-14 14:36:37 +0800

    提交代码

---
+++ 2022/test_10_14/test_10_14/test.c
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+// 计算一个数在内存中存储的二进制中1的个数
+//int main()
+//{
+//	int num = 0;
+//	int count = 0;
+//	scanf("%d", &num);
+//
+//	int i = 0;
+//	for (i = 0; i < 32; ++i)
+//	{
+//		if (((num >> i) & 1) == 1)
+//			++count;
+//	}
+//	printf("%d\n", count);
+//
+//	return 0;
+//}
+
+//#include <stdbool.h>
+//
+//int main()
+//{
+//	bool flag1 = true;
+//	if (flag1)
+//	{
+//		printf("true\n");
+//	}
+//
+//	bool flag2 = false;
+//	if (!flag2)
+//	{
+//		printf("false\n");
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	printf("%d\n", sizeof(a));
+//	printf("%d\n", sizeof a);
+//	printf("%d\n", sizeof(int));
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	short s = 0;
+//
+//	printf("%d\n", sizeof(s = a + 2));
+//	printf("%d\n", s);
+//
+//	return 0;
+//}
+
+//void test1(int arr[])
+//{
+//	printf("%d\n", sizeof(arr));
+//}
+//
+//void test2(char ch[])
+//{
+//	printf("%d\n", sizeof(ch));
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	char ch[10] = { 0 };
+//
+//	printf("%d\n", sizeof(arr));
+//	printf("%d\n", sizeof(ch));
+//
+//	test1(arr);
+//	test2(ch);
+//
+//	return 0;
+//}
+
