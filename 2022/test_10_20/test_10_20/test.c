888d511f149b7dea39d918c1560d7e096a86e942
bolinx2003 <3154756364@qq.com>
2022-10-20 20:13:45 +0800

    提交代码

---
+++ 2022/test_10_20/test_10_20/test.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a', 'b', 'c' };
+//
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//
+//	printf("%s\n", arr1);
+//	printf("%s\n", arr2);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%c\n", '\'');
+//	printf("%s\n", "\"");
+//
+//	printf("\a");
+//	
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 9;
+//	if (a >= 10)
+//	{
+//		printf("hehe\n");
+//	}
+//	else
+//	{
+//		printf("haha\n");
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int line = 0;
+//
+//	while (line<=20000)
+//	{
+//		++line;
+//		printf("ÇÃ´úÂë£º%d\n", line);
+//	}
+//
+//	if (line > 20000)
+//		printf("ºÃoffer\n");
+//
+//	return 0;
+//}
+
