5ae4e36994da2be6b25b661ff8dd6ca32d7e7e03
bolinx2003 <3154756364@qq.com>
2022-10-21 20:49:48 +0800

    提交代码

---
+++ 2022/test_10_21/test_10_21/test.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+// 判断大小端
+//int check_sys()
+//{
+//	int a = 1;
+//	return *(char*)&a;
+//}
+//
+//int main()
+//{
+//	int ret = check_sys();
+//	if (1 == ret)
+//	{
+//		printf("小端\n");
+//	}
+//	else
+//	{
+//		printf("大端\n");
+//	}
+//
+//	return 0;
+//}
