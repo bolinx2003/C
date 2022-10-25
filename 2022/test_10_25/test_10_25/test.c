24a7a5e3140227f5fbe479e4d1665c3066657c01
bolinx2003 <3154756364@qq.com>
2022-10-25 21:15:18 +0800

    提交代码

---
+++ 2022/test_10_25/test_10_25/test.c
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	// 常量字符串
+//	const char* p1 = "abcdef";
+//	const char* p2 = "abcdef";
+//	if (p1 == p2)
+//	{
+//		printf("p1 == p2\n"); // yes
+//	}
+//	else
+//	{
+//		printf("p1 != p2\n");
+//	}
+//
+//	// 数组
+//	char arr1[] = "abcdef";
+//	char arr2[] = "abcdef";
+//	if (arr1 == arr2)
+//	{
+//		printf("arr1 == arr2\n");
+//	}
+//	else
+//	{
+//		printf("arr1 != arr2\n"); // yes
+//	}
+//
+//	return 0;
+//}
