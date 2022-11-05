c3d7e81f2c40654c2b47b6b22e010d5bf850427d
bolinx2003 <3154756364@qq.com>
2022-11-05 22:10:39 +0800

    提交代码

--- 2022/test_11_5/test_11_5/test.c
+++ 2022/test_11_5/test_11_5/test.c
@@ -374,3 +374,321 @@
 //
 //	return 0;
 //}
+
+//void find_single_dog(int arr[], int sz)
+//{
+//	// 全部异或到一起
+//	int ret = 0;
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		ret ^= arr[i];
+//	}
+//	// 找出异或出来的数的二进制位中的1
+//	int pos = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if (((ret >> i) & 1) == 1)
+//		{
+//			pos = i;
+//			break;
+//		}
+//	}
+//	// 分组，把第pos位为1或者为0的分为一组，异或到一起
+//	int single1 = 0;
+//	int single2 = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		if (((arr[i] >> pos) & 1) == 1)
+//		{
+//			single1 ^= arr[i];
+//		}
+//	}
+//	single2 = ret ^ single1;
+//	printf("%d %d\n", single1, single2);
+//}
+//
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,1,2,3,4,6 };
+//	find_single_dog(arr, sizeof(arr) / sizeof(arr[0]));
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	// 打开文件
+//	FILE* pf = fopen("test.txt", "r");
+//	if (pf == NULL)
+//	{
+//		perror("fopen");
+//		return 1;
+//	}
+//
+//	// 读文件
+//	// ...
+//
+//	// 关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	// 打开文件
+//	FILE* pf = fopen("test.txt", "w");
+//	if (pf == NULL)
+//	{
+//		perror("fopen");
+//		return 1;
+//	}
+//
+//	// 写文件
+//	char ch = 0;
+//	for (ch = 'a'; ch <= 'z'; ch++)
+//	{
+//		fputc(ch, pf);
+//	}
+//
+//	// 关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	FILE* pf = fopen("test.txt", "r");
+//	if (pf == NULL)
+//	{
+//		perror("fopen");
+//		return 1;
+//	}
+//
+//	// 读文件
+//	//fgetc(pf);
+//	int ch = 0;
+//	while ((ch = fgetc(pf)) != EOF)
+//	{
+//		printf("%c ", ch);
+//		//putchar(ch);
+//	}
+//
+//	fclose(pf);
+//	pf = NULL;
+//
+//	return 0;
+//}
+
+//void test1()
+//{
+//	FILE* pf = fopen("test.txt", "w");
+//	if (pf == NULL)
+//	{
+//		perror("test1::fopen");
+//		return;
+//	}
+//
+//	// 写入A~Z
+//	char ch = 0;
+//	for (ch = 'A'; ch <= 'Z'; ch++)
+//	{
+//		fputc(ch, pf);
+//	}
+//
+//	fclose(pf);
+//	pf = NULL;
+//}
+//
+//void test2()
+//{
+//	FILE* pf = fopen("test.txt", "r");
+//	if (pf == NULL)
+//	{
+//		perror("test2::fopen");
+//		return;
+//	}
+//
+//	int ch = 0;
+//	while (~(ch = fgetc(pf)))
+//	{
+//		fputc(ch, stdout);
+//	}
+//
+//	fclose(pf);
+//	pf = NULL;
+//}
+//
+//int main()
+//{
+//	// 写文件
+//	test1();
+//
+//	// 读文件
+//	test2();
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int ch = fgetc(stdin);
+//	fputc(ch, stdout);
+//
+//	return 0;
+//}
+
+//void test_putc()
+//{
+//	// 打开文件
+//	FILE* pf = fopen("test.txt", "w");
+//	if (pf == NULL)
+//	{
+//		perror("test_putc::fopen");
+//		return;
+//	}
+//
+//	// 写文件
+//	// 写入a b c ... z
+//	char ch = 'a';
+//	for (ch = 'a'; ch <= 'z'; ch++)
+//	{
+//		fputc(ch, pf);
+//	}
+//
+//	// 关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//}
+//
+//void test_getc()
+//{
+//	// 打开文件
+//	FILE* pf = fopen("test.txt", "r");
+//	if (pf == NULL)
+//	{
+//		perror("test_getc::fopen");
+//		return;
+//	}
+//
+//	// 读文件
+//	int ch = 0;
+//	while (~(ch = fgetc(pf)))
+//	{
+//		fputc(ch, stdout);
+//	}
+//
+//	// 关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//}
+//
+//void test_puts()
+//{
+//	// 打开文件
+//	FILE* pf = fopen("test.txt", "w");
+//	if (pf == NULL)
+//	{
+//		perror("test_puts::fopen");
+//		return;
+//	}
+//
+//	// 写文件
+//	fputs("abcdefg\n", pf);
+//	fputs("ABCDEFG\n", pf);
+//	fputs("1234567890\n", pf);
+//
+//	// 关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//}
+//
+//void test_gets()
+//{
+//	// 打开文件
+//	FILE* pf = fopen("test.txt", "r");
+//	if (pf == NULL)
+//	{
+//		perror("test_gets::fopen");
+//		return;
+//	}
+//
+//	// 读文件
+//	char arr[1000] = { 0 };
+//	while (fgets(arr, 1000, pf))
+//	{
+//		printf("%s", arr);
+//	}
+//
+//	// 关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//}
+//
+//int main()
+//{
+//	//test_putc();
+//	//test_getc();
+//	test_puts();
+//	test_gets();
+//
+//	return 0;
+//}
+
+//struct Stu
+//{
+//	char name[20];
+//	int age;
+//	double score;
+//};
+//
+//void test_fprintf()
+//{
+//	// 打开文件
+//	FILE* pf = fopen("test2.txt", "w");
+//	if (pf == NULL)
+//	{
+//		perror("test_fprintf::fopen");
+//		return;
+//	}
+//
+//	// 写文件
+//	struct Stu s = { "张三", 18, 95.5 };
+//	fprintf(pf, "%s %d %lf\n", s.name, s.age, s.score);
+//
+//	// 关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//}
+//
+//void test_fscanf()
+//{
+//	// 打开文件
+//	FILE* pf = fopen("test2.txt", "r");
+//	if (pf == NULL)
+//	{
+//		perror("test_fscanf");
+//		return;
+//	}
+//
+//	// 读文件
+//	struct Stu s = { 0 };
+//	fscanf(pf, "%s %d %lf", s.name, &(s.age), &(s.score));
+//	fprintf(stdout, "%s %d %lf\n", s.name, s.age, s.score);
+//
+//	// 关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//}
+//
+//int main()
+//{
+//	test_fprintf();
+//	test_fscanf();
+//
+//	return 0;
+//}
