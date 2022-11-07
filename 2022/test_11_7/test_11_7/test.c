76738fa41b7d207b7ca900c51b13b570f3b79c19
bolinx2003 <3154756364@qq.com>
2022-11-07 17:58:18 +0800

    提交代码

--- 2022/test_11_7/test_11_7/test.c
+++ 2022/test_11_7/test_11_7/test.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <ctype.h>
+#include <stddef.h>
 
 //enum Status
 //{
@@ -86,6 +87,86 @@
 //		printf("正常转化：%d\n", ret);
 //	else
 //		printf("非法转化：%d\n", ret);
+//
+//	return 0;
+//}
+
+// 把空格替换成%20
+//#include <string.h>
+//
+//void ReplaceSpace(char* str, int len)
+//{
+//	assert(str);
+//
+//	// 统计空格的个数
+//	char* cur = str;
+//	int count = 0;
+//	while (len--)
+//	{
+//		if (isspace(*cur))
+//		{
+//			count++;
+//		}
+//		cur++;
+//	}
+//
+//	char* end1 = cur - 1;
+//	char* end2 = end1 + count * 2;
+//
+//	while (end1 < end2)
+//	{
+//		if (!isspace(*end1))
+//		{
+//			*end2-- = *end1--;
+//		}
+//		else
+//		{
+//			end1--;
+//			*end2-- = '0';
+//			*end2-- = '2';
+//			*end2-- = '%';
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	char arr[50] = "We are happy!";
+//	ReplaceSpace(arr, strlen(arr));
+//	printf("%s\n", arr);
+//
+//	return 0;
+//}
+
+// 写一个宏，交换一个数二进制位的奇数位和偶数位
+//#define SWAP(num) (num = (((num&0x55555555)<<1)+((num&0xaaaaaaaa)>>1)))
+//// 0101...0101 去掉偶数位，保留奇数位
+//// 1010...1010 去掉奇数位，保留偶数位
+//
+//int main()
+//{
+//	int num = 10; // 1010
+//	SWAP(num);    // 0101 -> 5
+//	printf("%d\n", num);
+//
+//	return 0;
+//}
+
+// 模拟实现offsetof
+//#define OFFSETOF(st_type, mem_name) ((size_t)&(((st_type*)0)->mem_name))
+//
+//struct S
+//{
+//	int i;
+//	char c;
+//	double d;
+//};
+//
+//int main()
+//{
+//	printf("%d\n", OFFSETOF(struct S, i));
+//	printf("%d\n", OFFSETOF(struct S, c));
+//	printf("%d\n", OFFSETOF(struct S, d));
 //
 //	return 0;
 //}
