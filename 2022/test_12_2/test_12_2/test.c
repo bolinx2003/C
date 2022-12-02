662126a9f41b3c45ff06563857811bb7c85cfff8
bolinx2003 <3154756364@qq.com>
2022-12-02 23:22:46 +0800

    提交代码

---
+++ 2022/test_12_2/test_12_2/test.c
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char passwd[20] = { 0 };
+	for (size_t i = 0; i < 3; i++)
+	{
+		printf("请输入密码:>");
+		scanf("%s", passwd);
+		if (strcmp(passwd, "passwd") == 0)
+		{
+			printf("登陆成功\n");
+			break;
+		}
+		else if (i == 2)
+		{
+			printf("三次密码均输入错误，登录失败\n");
+			break;
+		}
+		else
+		{
+			printf("密码错误，重新输入\n");
+		}
+	}
+
+	return 0;
+}
