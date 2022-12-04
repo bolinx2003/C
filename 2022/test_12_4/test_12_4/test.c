401262d5e23b0bca4272bcf738546c0974df60ae
bolinx2003 <3154756364@qq.com>
2022-12-04 19:17:41 +0800

    提交代码

---
+++ 2022/test_12_4/test_12_4/test.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool is_leap_year(int year)
+{
+	return ((year % 4 == 0) && (year % 100)) || (year % 400 == 0);
+}
+
+int main()
+{
+	for (int year = 100; year <= 200; year++)
+	{
+		if (is_leap_year(year))
+			printf("%d ", year);
+	}
+
+	return 0;
+}
