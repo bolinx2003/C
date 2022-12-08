b241a6937b1844a2d62636ffcb449a1af661412b
bolinx2003 <3154756364@qq.com>
2022-12-08 17:26:37 +0800

    提交代码

---
+++ 2022/test_12_8/test_12_8/test.c
@@ -0,0 +1,47 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+// 1 - 小端
+// 0 - 大端
+int check_sys1()
+{
+	int i = 1;
+	return *((char*)&i);
+}
+
+int check_sys2()
+{
+	union
+	{
+		int i;
+		char c;
+	}u;
+	u.i = 1;
+	return u.c;
+}
+
+int main()
+{
+	int ret = check_sys1();
+	if (ret)
+	{
+		printf("小端\n");
+	}
+	else
+	{
+		printf("大端\n");
+	}
+
+	ret = check_sys2();
+	if (ret)
+	{
+		printf("小端\n");
+	}
+	else
+	{
+		printf("大端\n");
+	}
+
+	return 0;
+}
