e08b900241aafed310888a78c9d137c4bcf9ec15
bolinx2003 <3154756364@qq.com>
2022-12-09 16:25:23 +0800

    提交代码

---
+++ 2022/test_12_9/test_12_9/test.c
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+int main()
+{
+	int* p = (int*)calloc(10, sizeof(int));
+	if (p == NULL)
+	{
+		printf("main::calloc: %s\n", strerror(errno));
+		return 1;
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d ", *(p + i));
+	}
+	printf("\n");
+
+	for (int i = 0; i < 10; i++)
+	{
+		p[i] = i;
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d ", p[i]);
+	}
+	printf("\n");
+
+	free(p);
+	p = NULL;
+
+	return 0l;
+}
