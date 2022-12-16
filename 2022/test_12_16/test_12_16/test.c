bbaca2b141281f32a593bee56855c518afc266a8
bolinx2003 <3154756364@qq.com>
2022-12-16 13:14:23 +0800

    提交代码

---
+++ 2022/test_12_16/test_12_16/test.c
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdbool.h>
+
+void bubble_sort(int* arr, int sz)
+{
+	for (int i = 0; i < sz - 1; i++)
+	{
+		bool flag = true;
+		for (int j = 0; j < sz - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				flag = false;
+				int tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+		if (flag)
+			return;
+	}
+}
+
+int main()
+{
+	int arr[] = { 10,9,8,7,6,5,4,3,2,1 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+
+	bubble_sort(arr, sz);
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+
+	return 0;
+}
