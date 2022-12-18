c3bd008bf297805ced020180a8ad30293d2fbd8a
bolinx2003 <3154756364@qq.com>
2022-12-18 17:14:04 +0800

    提交代码

---
+++ 2022/test_12_18/test_12_18/test.c
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+	srand((unsigned int)time(NULL));
+	int rand_num = rand();
+	printf("rand_num = %d\n", rand_num);
+
+	// 二分查找
+	int max = RAND_MAX;
+	int min = 0;
+	int count = 0;
+	while (1)
+	{
+		int mid = (max + min) / 2;
+		++count;
+		printf("第%d次查找: mid = %d , ", count, mid);
+		if (mid > rand_num)
+		{
+			printf("大了\n");
+			max = mid - 1;
+		}
+		else if (mid < rand_num)
+		{
+			printf("小了\n");
+			min = mid + 1;
+		}
+		else
+		{
+			printf("找到了\n");
+			break;
+		}
+	}
+
+	return 0;
+}
