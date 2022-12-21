eb737cf6808d12c6b78c67b46c9bfc5522b81122
bolinx2003 <3154756364@qq.com>
2022-12-21 18:53:20 +0800

    提交代码

---
+++ 2022/test_12_21/test_12_21/test.c
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+// 判断素数
+bool is_prime(int n)
+{
+	for (int i = 2; i <= sqrt(n); ++i)
+	{
+		if (n % i == 0)
+			return false;
+	}
+
+	return true;
+}
+
+// 打印并统计100~200之间素数的个数
+int main()
+{
+	int count = 0;
+	for (int i = 100; i <= 200; ++i)
+	{
+		if (is_prime(i))
+		{
+			++count;
+			printf("%d ", i);
+		}
+	}
+	printf("\ncount = %d\n", count);
+
+	return 0;
+}
