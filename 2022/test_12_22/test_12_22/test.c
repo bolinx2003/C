6d94088928b6c89fdd8c0b59414a0d16b4a3fe06
bolinx2003 <3154756364@qq.com>
2022-12-22 14:46:55 +0800

    提交代码

---
+++ 2022/test_12_22/test_12_22/test.c
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int* ptr = (int*)calloc(10, sizeof(int));
+	if (ptr == NULL)
+	{
+		perror("calloc");
+		return 1;
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		*(ptr + i) = i + 1;
+	}
+
+	for (int i = 0; i < 40; i++)
+	{
+		printf("%-2d ", *(((char*)ptr) + i));
+		if ((i + 1) % 4 == 0)
+			printf("\n");
+	}
+
+	free(ptr);
+	ptr = NULL;
+
+	return 0;
+}
