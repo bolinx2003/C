fddcd59a300ee3edabec9094561e0258dab1a890
bolinx2003 <3154756364@qq.com>
2022-12-24 17:28:13 +0800

    提交代码

---
+++ 2022/test_12_24/test_12_24/test.c
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+
+typedef struct my_struct2
+{
+	int* pi;
+	char str[20];
+}S2;
+
+typedef struct my_struct1
+{
+	int i;
+	double d;
+	char c;
+	S2 s;
+}S1;
+
+int main()
+{
+	S1 s;
+	s.i = 100;
+	s.d = 3.14;
+	s.c = 'w';
+	s.s.pi = &(s.i);
+	strcpy(s.s.str, "abc");
+
+	printf("%d %lf %c\n", s.i, s.d, s.c);
+	printf("%d %s\n", *(s.s.pi), s.s.str);
+
+	return 0;
+}
