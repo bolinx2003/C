2078527dfb9f9931e36339346fbd621fb3b39541
bolinx2003 <3154756364@qq.com>
2022-12-25 12:03:47 +0800

    提交代码

---
+++ 2022/test_12_25/test_12_25/test.c
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdbool.h>
+#include <stdio.h>
+
+// true - 小端
+// false - 大端
+
+//bool check_sys()
+//{
+//	int a = 1;
+//	return (bool)*(char*)&a;
+//}
+
+bool check_sys()
+{
+	union
+	{
+		int i;
+		char c;
+	}u;
+	u.i = 1;
+	return (bool)u.c;
+}
+
+int main()
+{
+	if (check_sys())
+		printf("小端\n");
+	else
+		printf("大端\n");
+
+	return 0;
+}
