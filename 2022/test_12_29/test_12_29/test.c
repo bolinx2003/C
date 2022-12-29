4543aa6807f6a5b5bc9a80e73d2c82e954fc01c8
bolinx2003 <3154756364@qq.com>
2022-12-29 16:42:15 +0800

    提交代码

---
+++ 2022/test_12_29/test_12_29/test.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+// 计算二阶行列式
+
+int main()
+{
+	printf("计算二阶行列式\n");
+
+	double arr[2][2] = { 0 };
+	printf("请输入第一行:>");
+	scanf("%lf %lf", &arr[0][0], &arr[0][1]);
+	printf("请输入第二行:>");
+	scanf("%lf %lf", &arr[1][0], &arr[1][1]);
+
+	printf("结果是: %lf\n", arr[0][0] * arr[1][1] - arr[0][1] * arr[1][0]);
+
+	return 0;
+}
